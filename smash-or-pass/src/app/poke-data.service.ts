b569dcaa6200bb37d619d41cf8b828642cc5a17d
Annie <anniepbenner@gmail.com>
2022-03-06 17:25:28 -0500

    Initial commit

---
+++ smash-or-pass/src/app/poke-data.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { PokemonClient } from 'pokenode-ts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeDataService {
+
+  constructor() { }
+}
